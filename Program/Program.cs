bfd6c7d63dddd62495cad765eab3af7a5c10ac44
Timofey <timal1@yandex.ru>
2022-12-10 22:43:55 +0300

    write code in Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿string[] arrayStringStart = { "hello", "2", "world", "!", "yes" };
+string[] arrayStringEnd = new string[arrayStringStart.Length];
+int index = 0;
+
+string[] ChangeArray(string[] arrayStringStart)
+{
+    for (int i = 0; i < arrayStringStart.Length; i++)
+        if (arrayStringStart[i].Length <= 3)
+        {
+            arrayStringEnd[index] = arrayStringStart[i];
+            index += 1;
+        }
+    return arrayStringEnd;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+ShowArray(ChangeArray(arrayStringStart));
